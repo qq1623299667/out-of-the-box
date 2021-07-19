b467377fcb5e5dc622581b1694de02c54146c48b
石佳will <5007929@medcaptain.cn>
2021-07-19 14:52:27 +0800

    实现自定义查用户名密码

---
+++ shiro/src/main/java/com/example/shiro/FirstRealm.java
@@ -0,0 +1,26 @@
+package com.example.shiro;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.AuthenticatingRealm;
+
+public class FirstRealm extends AuthenticatingRealm {
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        UsernamePasswordToken upToken = (UsernamePasswordToken)authenticationToken;
+        String userName = upToken.getUsername();
+        //假设这就是从数据库获取的用户信息
+        SimpleAccount simpleAccount = getAccountInfo();
+        if(simpleAccount.getPrincipals().asList().contains(userName)) {
+            System.out.println("用户名验证通过");
+            return simpleAccount;
+        }else{
+            return null;
+//            throw new UnknownAccountException();
+        }
+    }
+
+    private SimpleAccount getAccountInfo(){
+        return new SimpleAccount("zhaoyun","1111",this.getName());
+//        return  new SimpleAuthenticationInfo("zhaoyun","1111",this.getName());
+    }
+}
--- shiro/src/main/java/com/example/shiro/TestAuthenticator.java
+++ shiro/src/main/java/com/example/shiro/TestAuthenticator.java
@@ -13,12 +13,14 @@ public class TestAuthenticator {
         //第一步，获取Security Manager
         DefaultSecurityManager defaultSecurityManager = new DefaultSecurityManager();
         //第二步，使用Security Manager加载配置文件，注意前面已经说过Realm是身份认证与权限的数据获取的地方，所以调用setRealm
-        defaultSecurityManager.setRealm(new IniRealm("classpath:shiro.ini"));
+//        defaultSecurityManager.setRealm(new IniRealm("classpath:shiro.ini"));
+        defaultSecurityManager.setRealm(new FirstRealm());
         //第三步，使用SecurityUtils获取Subject
         SecurityUtils.setSecurityManager(defaultSecurityManager);
         Subject subject = SecurityUtils.getSubject();
         //第四步，获取用户登录Token
         UsernamePasswordToken authenticationToken = new UsernamePasswordToken("zhaoyun","daye");
+//        UsernamePasswordToken authenticationToken = new UsernamePasswordToken("zhaoyun","1111");
         try {
             System.out.println(subject.isAuthenticated());
             //第五步，登录
