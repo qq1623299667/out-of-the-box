57e5112e9113d298758920f8b8081e94f86fc9e1
石佳will <1623299667@qq.com>
2021-07-22 10:02:51 +0800

    登录，get请求，post请求

--- spider/src/main/java/com/example/demo/HttpRequest.java
+++ spider/src/main/java/com/example/demo/HttpRequest.java
@@ -9,33 +9,63 @@ import java.util.Map;
 import java.util.Set;
 
 public class HttpRequest {
+    public static Map<String, String> cookies;
+
     public static void main(String[] args) throws IOException {
         // 登录
         String url = "http://localhost:8009/login";
         String requestBody = "{\"userName\":\"admin\",\"password\":\"123456\"}";
-        Connection.Response res = postLogin(url,requestBody);
+        String login = postLogin(url, requestBody);
+        System.out.println(login);
 
-        Document doc = res.parse();
+        // 获取用户列表
+        String url1="http://localhost:8009/user/userList";
+        String text1 = get(url1);
+        System.out.println(text1);
+
+        // 获取病人历史
+        String url2="http://localhost:8009/queryPatientTestHistory";
+        String requestBody2 = "{\"currentPage\":1,\"sizePage\":10,\"personId\":\"b1b0e8150a2a4cbc8a64\"}";
+        String text2 = post(url2,requestBody2);
+        System.out.println(text2);
+    }
 
 
-        System.out.println(doc.text());
-        //这儿的SESSIONID需要根据要登录的目标网站设置的session Cookie名字而定
-        Map<String, String> map = res.cookies();
-        Set<Map.Entry<String, String>> entries = map.entrySet();
-        Connection connection = Jsoup.connect("http://localhost:8009/user/userList").ignoreContentType(true);
+    // get请求数据
+    public static String get(String url1) throws IOException {
+        Connection connection = Jsoup.connect(url1).ignoreContentType(true);
+        Set<Map.Entry<String, String>> entries = cookies.entrySet();
         for(Map.Entry<String, String> entry:entries){
             connection.cookie(entry.getKey(), entry.getValue());
         }
         Document objectDoc = connection.get();
-        System.out.println(objectDoc.text());
+        return objectDoc.text();
     }
 
-    private static Connection.Response postLogin(String url,String requestBody) throws IOException {
-        return Jsoup.connect(url)
+    // post登录
+    public static String postLogin(String url,String requestBody) throws IOException {
+        Connection.Response execute = Jsoup.connect(url)
                 .requestBody(requestBody)
-                .header("Content-Type","application/json")
+                .header("Content-Type", "application/json")
                 .ignoreContentType(true)
                 .method(Connection.Method.POST)
                 .execute();
+        cookies = execute.cookies();
+        return execute.parse().text();
+    }
+
+    // post请求数据
+    public static String post(String url,String requestBody) throws IOException {
+        Connection connection = Jsoup.connect(url)
+                .requestBody(requestBody)
+                .header("Content-Type", "application/json")
+                .ignoreContentType(true)
+                .method(Connection.Method.POST);
+        Set<Map.Entry<String, String>> entries = cookies.entrySet();
+        for(Map.Entry<String, String> entry:entries){
+            connection.cookie(entry.getKey(), entry.getValue());
+        }
+        Connection.Response execute = connection.execute();
+        return execute.parse().text();
     }
 }
